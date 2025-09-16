ace74df68d73efdda72e2b24a145232a6d38d544
jonous10 <jonous10@imkatta.no>
2025-09-16 09:15:10 +0200

    add hamburger and donate heart

--- habitat-for-humanity/src/components/ui/hh-header.tsx
+++ habitat-for-humanity/src/components/ui/hh-header.tsx
@@ -1,5 +1,9 @@
 import { Button } from "./button";
-
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
 export function HH_Header() {
   return (
@@ -26,6 +30,19 @@ export function HH_Header() {
       {/*MOBILE FORMAT*/}
       <div className="max-w-6xl mx-auto px-4 py-4 items-center justify-between flex sm:hidden">
         <h1 className="text-1xl font-bold text-gray-800">Habitat for Humanity</h1>
+        <Button className="bg-red-600">
+          <p className="font-bold text-xl">Donate</p>
+          <i className="fa-solid fa-heart fa-beat"></i>
+        </Button>
+        {/* HAMBURGET MENU */}
+        <Popover>
+          <PopoverTrigger asChild>
+            <i className="fa-solid fa-bars cursor-pointer fa-xl"></i>
+          </PopoverTrigger>
+          <PopoverContent className="w-80">
+            
+          </PopoverContent>
+        </Popover>
       </div>
     </header>
   )
