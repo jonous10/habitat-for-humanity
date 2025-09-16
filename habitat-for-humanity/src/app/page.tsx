e793afb5aaa679f5566ec3a67464eba3e1bb6c6c
jonous10 <jonous10@imkatta.no>
2025-09-16 13:28:27 +0200

    add image hover effect

--- habitat-for-humanity/src/app/page.tsx
+++ habitat-for-humanity/src/app/page.tsx
@@ -8,15 +8,28 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* HEADER */}
-        <HH_Header/>
-
+      <HH_Header />
+      <div className="
+      sm:mx-auto
+      group
+      ">
         <img
           src="/images/hh-family-expand.jpg"
-          className="relative h-62 hover:h-190 w-full object-cover transition-all duration-200 sm:(rounded-full)"
+          className="
+            relative h-62 w-full my-10 object-cover transition-all duration-500
+            sm:rounded-[50%] sm:w-100 sm:h-100
+            group-hover:rounded-[0%]
+            group-hover:w-480
+            group-hover:h-190
+            group-hover:my-0
+            group-hover:[transform:rotateY(180deg)]
+            transform [transform-style:preserve-3d]
+            "
           alt="Habitat for Humanity Family Photo"
-        />
-
-        {/* FOOTER */}
+          />
+          <img src=""/>
+      </div>
+      
       <HH_Footer/>
     </div>
   );
